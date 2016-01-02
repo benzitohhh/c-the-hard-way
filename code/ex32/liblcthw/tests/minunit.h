c63e90eee50aee159e9a43ba7a43e5fb3ae36199
U-Hobbes-PC\Hobbes <immanuel_ben@yahoo.co.uk>
2016-01-02 17:09:26 -0300

    linkedlist and tests ok

--- code/ex32/liblcthw/tests/minunit.h
+++ code/ex32/liblcthw/tests/minunit.h
@@ -13,20 +13,20 @@
     message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    argc = 1; \
+    argc = 1;\
+    printf("argc: %d\n", argc); /* to stop compiler complaining about unnused arg */ \
     debug("----- RUNNING: %s", argv[0]);\
-        printf("----\nRUNNING: %s\n", argv[0]);\
-        char *result = name();\
-        if (result != 0) {\
-            printf("FAILED: %s\n", result);\
-        }\
-        else {\
-            printf("ALL TESTS PASSED\n");\
-        }\
+    printf("----\nRUNNING: %s\n", argv[0]);\
+    char *result = name();\
+    if (result != 0) {\
+        printf("FAILED: %s\n", result);\
+    }\
+    else {\
+        printf("ALL TESTS PASSED\n");\
+    }\
     printf("Tests run: %d\n", tests_run);\
-        exit(result != 0);\
+    exit(result != 0);\
 }
-
 
 int tests_run;
 
